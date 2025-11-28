6ccebc014079bf346ff4745161e44f9615b639ed
Vlasova Mariia <vlasovam014@gmail.com>
2025-11-28 14:19:03 +0300

    create project

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CatTinder());
+}
+
+class CatTinder extends StatelessWidget {
+  const CatTinder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Кототиндер',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.black87,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+      home: const MainTabScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainTabScreen extends StatefulWidget {
+  const MainTabScreen({super.key});
+  final String title = 'Кототиндер';
+
+  @override
+  State<MainTabScreen> createState() => _MainTabScreenState();
+}
+
+class _MainTabScreenState extends State<MainTabScreen> {
+  int _currentIndex = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _currentIndex++;
+    });
+  }
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const BreedsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ),
+      body: _screens[_currentIndex],
+      
+    );
+  }
+}
